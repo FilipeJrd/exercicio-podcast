fa00ccf8d44e37d1f6dc083013b056b645ee4227
Filipe Nogueira Jordão <filipejrd@hotmail.com>
2017-10-15 22:21:18 -0300

    adding more values to details screen

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
@@ -11,18 +11,21 @@ import br.ufpe.cin.if710.podcast.domain.ItemFeed;
 public class EpisodeDetailActivity extends Activity {
     private TextView title;
     private TextView description;
+    private TextView link;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_episode_detail);
         this.title = findViewById(R.id.item_title);
         this.description = findViewById(R.id.item_description);
+        this.link = findViewById(R.id.item_link);
         //TODO preencher com informações do episódio clicado na lista...
         Bundle bundle = getIntent().getExtras();
         ItemFeed item = (ItemFeed) bundle.getSerializable("item");
         if(item != null){
             this.description.setText("Description: "+item.getDescription());
             this.title.setText("Title: "+item.getTitle());
+            this.link.setText("Link: "+item.getLink());
         }
     }
 }
